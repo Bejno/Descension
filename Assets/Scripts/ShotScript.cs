6b761c0d9f037fd10e32947eef48f9c095702d87
Bejno <bejnofalk@gmail.com>
2016-01-09 00:54:25 +0100

    New Logo
    - Added and made new logo

--- Assets/Scripts/ShotScript.cs
+++ Assets/Scripts/ShotScript.cs
@@ -3,11 +3,6 @@ using System.Collections;
 
 public class ShotScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-
     void OnTriggerEnter2D(Collider2D other)
     {
         var health = other.GetComponent<HealthScript>();
