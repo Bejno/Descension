731333d238a16a9c50dc9c4c7f52d6c5e459dfc9
Bejno <bejnofalk@gmail.com>
2016-01-11 10:35:03 +0100

    Tweaking
    - Stuff

--- Assets/Scripts/Bosses/Nun Boss/NunBossDeathScript.cs
+++ Assets/Scripts/Bosses/Nun Boss/NunBossDeathScript.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class NunBossDeathScript : MonoBehaviour {
 
+    public SpriteRenderer bell;
+    public Animator anim;
+
     public void DeathTrigger()
     {
+        
         LevelTimer.playerDead = false;
         AimScript.playerDead = false;
 
@@ -32,7 +36,32 @@ public class NunBossDeathScript : MonoBehaviour {
             script.delay();
         }
 
+        anim.SetTrigger("Burn");
+
+        SoundManagerScript.PlayHolyBellSound();
+
+        StartCoroutine(Bell());
+
         StartCoroutine(selfKill());
+    }
+
+    IEnumerator Bell()
+    {
+        bell.color = new Color(0.30f, 0.30f, 0.30f, 0.30f);
+        yield return new WaitForSeconds(0.1f);
+        bell.color = new Color(0.50f, 0.50f, 0.50f, 0.50f);
+        yield return new WaitForSeconds(0.1f);
+        bell.color = new Color(0.70f, 0.70f, 0.70f, 0.70f);
+        yield return new WaitForSeconds(0.1f);
+        bell.color = new Color(0.90f, 0.90f, 0.90f, 0.90f);
+        yield return new WaitForSeconds(0.1f);
+        bell.color = new Color(1f, 1f, 1f, 1f);
+        yield return new WaitForSeconds(0.3f);
+        bell.color = new Color(0.90f, 0.90f, 0.90f, 0.90f);
+        yield return new WaitForSeconds(0.1f);
+        bell.color = new Color(0.50f, 0.50f, 0.50f, 0.50f);
+        yield return new WaitForSeconds(0.1f);
+        bell.color = new Color(0.30f, 0.30f, 0.30f, 0.30f);
     }
 
     IEnumerator selfKill()
